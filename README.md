97b2da74718aa1ed26466aab7722f2565ee01b82
jacobjohn2016 <kapsjacob@gmail.com>
2020-04-23 01:06:02 +0530

    completed assignment

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # CleanTech Assignment
 
-Assignment for induction
+## Dependencies
+
+* Python 3.7+
+* pandas==1.0.3
+* numpy==1.18.3
+* Jupyter Notebook (to view EDA)
+
+## Running script
+
+### With default parameters
+
+```bash
+python3 clean_data.py
+```
+
+### With customer parameters
+
+```bash
+python3 clean_data.py data submission i32 m63 w23
+```
---
+++ clean_data.py
@@ -0,0 +1,88 @@
+# importing libraries
+
+import os
+import sys
+import time
+
+import pandas as pd
+import numpy as np
+
+
+def ensure_dir(dir_path):
+    """
+    Check if directory exists, else create it.
+
+    Keyword arguments:
+    dir_path -- directory path
+    """
+    directory = os.path.dirname(dir_path)
+    if not os.path.exists(directory):
+        print('Directory Exception: ' + dir_path +
+              ' not available, creating now...')
+        os.makedirs(directory)
+
+
+def change_timestamp(source_file_path, dest_file_path, ts_colnames):
+    """
+    Read file, change column name to timestamp and save it to new destination
+
+    Keyword arguments:
+    source_file_path -- file to read from
+    dest_file_path -- file to read into
+    ts_columns -- list of column names to update
+    """
+    # reading file as a dataframe
+    file = pd.read_csv(source_file_path, sep='\t')
+
+    # changing respective column name to Timestamp
+    cols = np.array(file.columns)
+    cols[file.columns.isin(ts_colnames)] = 'Timestamp'
+    file.columns = cols
+
+    # moving timestamp to first column
+    cols = list(cols)
+    cols.insert(0, cols.pop(cols.index('Timestamp')))
+    file = file[cols]
+
+    # saving file in new directory
+    file.to_csv(dest_file_path, index=False, sep='\t')
+
+
+def traversal_modify(source='data', destination='submission', ts_colnames=['i32', 'm63', 'w23']):
+    """
+    Traverse the source folder, modify the column names then store resulting file in destination folder
+
+    Keyword arguments:
+    source -- directory to copy from
+    destination -- directory to copy to
+    ts_colnames -- list of column names to update
+    """
+    for root, dirs, files in os.walk(source):
+
+        # skipping all hidden files
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
+        # dirs return empty when on leaf of folder structure
+        if not dirs:
+            root_dest = root.replace(source, destination)
+
+            # ensure directory exists
+            ensure_dir(root_dest + '/')
+
+            for f in files:
+                # modifying file
+                change_timestamp(source_file_path=root + "/" + f,
+                                 dest_file_path=root_dest + "/" + f,
+                                 ts_colnames=ts_colnames)
+
+if __name__ == "main":
+    start = time.time()
+    traversal_modify(source = sys.argv[0], destination = sys.argv[1], ts_colnames = sys.argv[2:])
+    end = time.time()
+    print("Successfully executed in {:.2f}s".format(end - start))
+else:
+    start = time.time()
+    traversal_modify()
+    end = time.time()
+    print("Successfully executed in {:.2f}s".format(end - start))
